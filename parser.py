52230b0b885702305a12c6f150ebdcdece0f61b8
rjcf18 <ricardo.fusco2@gmail.com>
2017-06-24 14:37:32 +0100

    test change

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ from datetime import *
 
 import numpy
 
-# loads the json file data
+#loads the json file data
 def loadJSON(file):
     with open(file, 'r') as myfile:
         data = myfile.read()
